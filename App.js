4330de4c9ae6aa6787c3d9d18e66aac2e641e363
DharaniKumar155 <dharanishdharani155@gmail.com>
2022-04-28 12:46:37 +0530

    Task6

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 
-export default function App() {
+const UselessTextInput = () => {
+  const [text] = React.useState(null);
+  const [password] = React.useState(null);
+  const [email] = React.useState(null);
+
+  const [number, onChangeNumber] = React.useState(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        value={text}
+        placeholder="Name"
+
+
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Mobile Number"
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        value={text}
+        placeholder="Email"
+        placeholderTextColor="red"
+        keyboardType="email-address"
+      />
+       <TextInput
+        style={styles.input}
+        value={text}
+        placeholder="Password"
+        placeholderTextColor="#9a73ef"
+        keyboardType="default"
+        returnKeyType='go'
+        secureTextEntry
+        autoCorrect={false}
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    marginTop:50,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
+
+export default UselessTextInput;
