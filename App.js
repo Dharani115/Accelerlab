46d998b345798e92a2ede6e3f4d03175f6681e81
DharaniKumar155 <dharanishdharani155@gmail.com>
2022-04-28 16:17:52 +0530

    Task7

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.text}>
+        Welcome to the very start of your React Native journey!
+         If you're looking for environment setup instructions, 
+         they've moved to their own section. Continue reading for
+          an introduction to the documentation, Native Components,
+          React, and more!
+          </Text>
+          <Text style={styles.text}>
+          Many different kinds of people use React Native: from 
+          advanced iOS developers to React beginners, to people 
+          getting started programming for the first time in their 
+          career. These docs were written for all learners, no matter 
+          their experience level or background.
+          </Text>
+          <Text style={styles.text}>
+          How to use these docs
+          You can start here and read through these docs linearly 
+          like a book; or you can read the specific sections you need. 
+          Already familiar with React? You can skip that section—or 
+          read it for a light refresher.
+
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
+  },
+  text: {
+    fontSize: 42,
+    paddingTop: 50,
   },
 });
+
+export default App;
