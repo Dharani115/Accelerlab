d3162aa7494b059a8e762a2b52cf95a5ef5a6cff
DharaniKumar155 <dharanishdharani155@gmail.com>
2022-05-20 12:18:38 +0530

    Task34

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  Platform,
+  PlatformColor,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+  <View style={styles.container}>
+    <Text style={styles.label}>
+      I am a special label color!
+    </Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
+  label: {
+    padding: 16,
+    ...Platform.select({
+      ios: {
+        color: PlatformColor('label'),
+        backgroundColor:
+          PlatformColor('systemTealColor'),
+      },
+      android: {
+        color: PlatformColor('?android:attr/textColor'),
+        backgroundColor:
+          PlatformColor('@android:color/holo_blue_bright'),
+      },
+      default: { color: 'black' }
+    })
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
+
+export default App;
